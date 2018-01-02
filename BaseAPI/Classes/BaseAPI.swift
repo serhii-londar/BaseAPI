e12ba1b9de5c272a7ddc82a9fbd78e2b28b38491
Serhii Londar <serhii.londar@gmail.com>
2018-01-02 20:51:50 +0200

    Make Base class open
    Add PUT and DELETE methods

--- BaseAPI/Classes/BaseAPI.swift
+++ BaseAPI/Classes/BaseAPI.swift
@@ -93,7 +93,7 @@ public class Request {
     }
 }
 
-public class BaseAPI {
+open class BaseAPI {
     public func get(url: String, parameters: [String : String]? = nil, headers: [String: String]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) {
         let request = Request(url: url, method: .GET, parameters: parameters, headers: headers, body: nil)
         let buildRequest = request.request()
@@ -126,4 +126,30 @@ public class BaseAPI {
             completion(nil, nil, buildRequest.1)
         }
     }
+    
+    
+    public func put(url: String, parameters: [String : String]? = nil, headers: [String: String]? = nil, body: Data?, completion: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) {
+        let request = Request(url: url, method: .PUT, parameters: parameters, headers: headers, body: body)
+        let buildRequest = request.request()
+        if let urlRequest = buildRequest.0 {
+            let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: completion)
+            task.resume()
+        } else {
+            completion(nil, nil, buildRequest.1)
+        }
+    }
+    
+    
+    public func delete(url: String, parameters: [String : String]? = nil, headers: [String: String]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) {
+        let request = Request(url: url, method: .DELETE, parameters: parameters, headers: headers)
+        let buildRequest = request.request()
+        if let urlRequest = buildRequest.0 {
+            let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: completion)
+            task.resume()
+        } else {
+            completion(nil, nil, buildRequest.1)
+        }
+    }
+    
+    
 }
