ef3a7ffa3559b779321c4df614474bd72335bdef
Serhii Londar <serhii.londar@gmail.com>
2018-01-05 03:04:52 +0200

    Create separate files
    Add url encoding

--- BaseAPI/Classes/BaseAPI.swift
+++ BaseAPI/Classes/BaseAPI.swift
@@ -1,97 +1,11 @@
 //
 //  BaseAPI.swift
-//  Alamofire
+//  BaseAPI
 //
 //  Created by Serhii Londar on 12/8/17.
 //
 
 import Foundation
-
-extension String: Error {
-    
-}
-
-public enum RequestMethod: String {
-    case OPTIONS
-    case GET
-    case HEAD
-    case POST
-    case PUT
-    case DELETE
-    case TRACE
-    case CONNECT
-}
-
-public enum RequestHeaderFields: String {
-    case acceptCharset = "Accept-Charset"
-    case acceptEncoding = "Accept-Encoding"
-    case acceptLanguage = "Accept-Language"
-    case authorization = "Authorization"
-    case expect = "Expect"
-    case from = "From"
-    case host = "Host"
-    case ifMatch = "If-Match"
-    case ifModifiedSince = "If-Modified-Since"
-    case ifNoneMatch = "If-None-Match"
-    case ifRange = "If-Range"
-    case ifUnmodifiedSince = "If-Unmodified-Since"
-    case maxForwards = "Max-Forwards"
-    case proxyAuthorization = "Proxy-Authorization"
-    case range = "Range"
-    case referer = "Referer"
-    case tE = "TE"
-    case userAgent = "User-Agent"
-}
-
-public class Request {
-    public var url: String
-    public var method: RequestMethod
-    public var parameters: [String : String]?
-    public var headers: [String : String]?
-    public var body: Data?
-    
-    public init(url: String, method: RequestMethod, parameters: [String : String]? = nil, headers: [String : String]? = nil, body: Data? = nil) {
-        self.url = url
-        self.method = method
-        self.parameters = parameters
-        self.headers = headers
-        self.body = body
-    }
-    
-    public func request() -> (URLRequest?, Error?) {
-        let url = URL(string: self.urlWithParameters())
-        if let url = url {
-            var request = URLRequest(url: url)
-            if let headers = headers {
-                for headerKey in headers.keys {
-                    request.addValue(headerKey, forHTTPHeaderField: headers[headerKey]!)
-                }
-            }
-            request.httpMethod = method.rawValue
-            request.httpBody = body
-            return (request, nil)
-        } else {
-            return (nil, "Unable to create URL")
-        }
-    }
-    
-    func urlWithParameters() -> String {
-        var retUrl = url
-        if let parameters = parameters {
-            if parameters.count > 0 {
-                retUrl.append("?")
-                for key in parameters.keys {
-                    guard let value = parameters[key] else {
-                        continue
-                    }
-                    retUrl.append("\(key)=\(value)&")
-                }
-                retUrl.removeLast()
-            }
-        }
-        return retUrl
-    }
-}
 
 open class BaseAPI {
     public init() {
@@ -143,7 +57,6 @@ open class BaseAPI {
         }
     }
     
-    
     public func delete(url: String, parameters: [String : String]? = nil, headers: [String: String]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) {
         let request = Request(url: url, method: .DELETE, parameters: parameters, headers: headers)
         let buildRequest = request.request()
@@ -154,6 +67,4 @@ open class BaseAPI {
             completion(nil, nil, buildRequest.1)
         }
     }
-    
-    
 }
---
+++ BaseAPI/Classes/RequestMethod.swift
@@ -0,0 +1,19 @@
+//
+//  RequestMethod.swift
+//  BaseAPI
+//
+//  Created by Serhii Londar on 1/5/18.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case OPTIONS
+    case GET
+    case HEAD
+    case POST
+    case PUT
+    case DELETE
+    case TRACE
+    case CONNECT
+}
